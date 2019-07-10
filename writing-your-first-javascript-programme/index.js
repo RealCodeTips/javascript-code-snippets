5bbff20649931d3c389786cb7cb06e9417a70675
RealCodeTips <simondrake1990+codetipsgithub@gmail.com>
2019-07-10 18:31:40 +0100

    initial commit

---
+++ writing-your-first-javascript-programme/index.js
@@ -0,0 +1,12 @@
+var name = "Simon"
+var age = 29
+var profession = "Developer"
+
+// String substitution
+console.log(
+  "Welcome to my first JavaScript programme. My name is %s. I am %d years old. I work as a %s", 
+  name, age, profession
+)
+
+// Template literal
+console.log(`Welcome to my first JavaScript programme. My name is ${name}. I am ${age} years old. I work as a ${profession}.`)
